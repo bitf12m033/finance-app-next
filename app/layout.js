aa3be970351c6b734e36d7f2fb13dd44da2ed3ab
Umer Majeed <umajeed@vinsuite.com>
2024-05-31 16:13:41 +0500

    Dark Mode Toggle

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import useServerDarkMode from "@/hooks/use-server-dark-mode";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,8 +10,9 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const theme = useServerDarkMode()
   return (
-    <html lang="en">
+    <html lang="en" className={theme}>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,13 +1,17 @@
 import Link from 'next/link'
+import DarkModeToggle from '../darkModetoggle/darkModeToggle'
+import useServerDarkMode from '@/hooks/use-server-dark-mode'
 
 const Header = ({className}) => {
+  const theme = useServerDarkMode()
   return (
     <header className={`flex justify-between items-center ${className}`}>
         <Link href={'/dashboard'} className='text-xl  hover:underline underline-offset-8 decoration-2'>
             Finance App
         </Link>
         <div className="flex items-center space-x-4">
-            <div>Mode Toggle</div>
+           <DarkModeToggle defaultMode= {theme}/>
+            
             <div>User Dropdown</div>
         </div>
 
