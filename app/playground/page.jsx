ed5efa1376e9532e108d287a255b4e4262a3999c
Umer Majeed <umajeed@vinsuite.com>
2024-05-31 15:25:34 +0500

    Transaction Summary

--- app/playground/page.jsx
+++ app/playground/page.jsx
@@ -1,29 +1,55 @@
 import Header from "@/components/header/header"
+import TransactionItem from "@/components/transactionItem/transactionItem"
+import TransactionSummaryItem from "@/components/transactionSummaryItem/transactionSummaryItem"
 import Trend from "@/components/trend/trend"
 const PlaygroundPage = () => {
   return (
     <main className="space-y-8">
-    <h1 className="text-4xl mt-8">Playground</h1>
+        <h1 className="text-4xl mt-8">Playground</h1>
 
-    <div>
-      <h2 className="mb-4 text-lg font-mono">ComponentName</h2>
-      <hr className="mb-4 border-gray-200 dark:border-gray-800" />
-      <div>
-        <Header />
-      </div>
-    </div>
+        <div>
+            <h2 className="mb-4 text-lg font-mono">ComponentName</h2>
+            <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+            <div>
+            <Header />
+            </div>
+        </div>
 
-    <div>
-        <h2 className="mb-4 text-lg font-mono">Trend</h2>
+        <div>
+            <h2 className="mb-4 text-lg font-mono">Trend</h2>
+            <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+            <div className="flex space-x-8">
+                <Trend type="Income" amount={1000} prevAmount={900} />
+                <Trend type="Expense" amount={12000} prevAmount={10000} />
+                <Trend type="Investment" amount={7000} prevAmount={11100} />
+                <Trend type="Saving" amount={500} prevAmount={950} />
+            </div>
+        </div>
+        
+        <div>
+        <h2 className="mb-4 text-lg font-mono">TransactionItem</h2>
         <hr className="mb-4 border-gray-200 dark:border-gray-800" />
-        <div className="flex space-x-8">
-          <Trend type="Income" amount={1000} prevAmount={900} />
-          <Trend type="Expense" amount={12000} prevAmount={10000} />
-          <Trend type="Investment" amount={7000} prevAmount={11100} />
-          <Trend type="Saving" amount={500} prevAmount={950} />
+        <div className="space-y-4">
+          <TransactionItem type="Income" description="Salary" amount={2000} />
+          <TransactionItem type="Expense" category="Food" description="Going out to eat" amount={29} />
+          <TransactionItem type="Saving" description="For children" amount={500} />
+          <TransactionItem type="Investment" description="In Microsoft" amount={9000} />
         </div>
       </div>
-  </main>
+
+      <div>
+        <h2 className="mb-4 text-lg font-mono">TransactionSummaryItem + TransactionItem</h2>
+        <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+        <div className="space-y-4">
+          <TransactionSummaryItem date="2024-05-01" amount={3500} />
+          <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+          <TransactionItem type="Income" description="Salary" amount={2000} />
+          <TransactionItem type="Expense" category="Food" description="Going out to eat" amount={29} />
+          <TransactionItem type="Saving" description="For children" amount={500} />
+          <TransactionItem type="Investment" description="In Microsoft" amount={9000} />
+        </div>
+      </div>
+    </main>
   )
 }
 
