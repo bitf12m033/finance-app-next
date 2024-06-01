02d2b16f71e8657c4073907ab284473e5c2dca17
Umer Majeed <umajeed@vinsuite.com>
2024-06-01 15:05:45 +0500

    Add variant page

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,11 +2,17 @@ import TransactionListFallback from '@/components/dashboard/transactionListFallb
 import Trend from '@/components/dashboard/trend/trend'
 import TrendFallback from '@/components/dashboard/trendFallback/trendFallback'
 import TransactionList from '@/components/transactionList/transactionList'
-import React, { Suspense } from 'react'
+import { PlusCircle } from 'lucide-react'
+import Link from 'next/link'
+import { Suspense } from'react'
+import { variants, sizes } from '@/lib/variants'
 
 const DashboardPage = () => {
   return (
     <>
+      <section className="mb-8">
+        <h1 className="text-4xl font-semibold">Summary</h1>
+      </section>
       <section className="mb-8 grid grid-cols-2 lg:grid-cols-4 gap-8">
       <Suspense fallback={<TrendFallback/>}>
         <Trend type="Income" />
@@ -22,6 +28,13 @@ const DashboardPage = () => {
       </Suspense>
     </section>
 
+    <section className="flex justify-between items-center mb-8">
+      <h2 className="text-2xl">Transactions</h2>
+      <Link href="/dashboard/transaction/add" className={`flex items-center space-x-1 ${variants['outline']} ${sizes['sm']}`}>
+        <PlusCircle className="w-4 h-4" />
+        <div>Add</div>
+      </Link>
+    </section>
     <Suspense fallback={<TransactionListFallback />}>
       <TransactionList />
     </Suspense>
---
+++ app/dashboard/transaction/add/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransactionAddPage = () => {
+    return <>
+    <h1 className="text-4xl font-semibold mb-8">Add Transaction</h1>
+  </>
+}
+
+export default TransactionAddPage
