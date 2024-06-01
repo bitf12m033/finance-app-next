33727c30ab9d7afdac6d60f20fb72c27905a76bc
Umer Majeed <umajeed@vinsuite.com>
2024-06-01 14:50:33 +0500

    Fetch Finance Trends , added trends

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,12 +1,30 @@
 import TransactionListFallback from '@/components/dashboard/transactionListFallback/transactionListFallback'
+import Trend from '@/components/dashboard/trend/trend'
 import TransactionList from '@/components/transactionList/transactionList'
 import React, { Suspense } from 'react'
 
 const DashboardPage = () => {
   return (
+    <>
+      <section className="mb-8 grid grid-cols-2 lg:grid-cols-4 gap-8">
+      <Suspense>
+        <Trend type="Income" />
+      </Suspense>
+      <Suspense>
+        <Trend type="Expense" />
+      </Suspense>
+      <Suspense>
+        <Trend type="Saving" />
+      </Suspense>
+      <Suspense>
+        <Trend type="Investment" />
+      </Suspense>
+    </section>
+
     <Suspense fallback={<TransactionListFallback />}>
       <TransactionList />
     </Suspense>
+    </>
   )
 }
 
