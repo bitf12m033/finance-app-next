1a49f76eec0dc25dab7fb03901949358bada981a
Umer Majeed <umajeed@vinsuite.com>
2024-06-05 21:49:19 +0500

    making category field optional for Income

--- components/dashboard/transactionForm/transactionForm.jsx
+++ components/dashboard/transactionForm/transactionForm.jsx
@@ -17,17 +17,21 @@ export default function TransactionForm() {
         register,
         handleSubmit,
         watch,
+        setValue,
         formState: { errors },
       } = useForm({
         mode: "onTouched",
-        // resolver: zodResolver(transactionSchema),
+        resolver: zodResolver(transactionSchema),
       })
     
     const router = useRouter()
     const [isSaving, setSaving] = useState(false)
     const [lastError, setLastError] = useState()
+    const type = watch('type')
 
     const onSubmit = async (data) => {
+      console.log(data)
+      return
         setSaving(true)
         setLastError()
         try {
@@ -47,14 +51,21 @@ export default function TransactionForm() {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div>
         <Label className="mb-1">Type</Label>
-        <Select {...register("type")}>
+        <Select {...register("type" ,{
+          onChange: (e) => {
+            if (e.target.value !== "Expense") {
+              setValue("category", "")
+            }
+          }
+        })}>
           {types.map(type => <option key={type}>{type}</option>)}
         </Select>
       </div>
 
       <div>
         <Label className="mb-1">Category</Label>
-        <Select {...register("category")}> 
+        <Select {...register("category")} disabled={type !=='Expense'}>
+          <option value=''>Select a category</option> 
           {categories.map(category => <option key={category}>{category}</option>)}
         </Select>
       </div>
