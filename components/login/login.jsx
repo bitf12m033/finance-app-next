61624fa860badfb55a55c5f967530b618fa02479
Umer Majeed <umajeed@vinsuite.com>
2024-06-06 18:51:23 +0500

    Laoding button for Login form

--- components/login/login.jsx
+++ components/login/login.jsx
@@ -1,10 +1,11 @@
-import Button from "../button/button";
+'use client'
+import SubmitButton from "../submitButton/submitButton";
 import Input from "../input/input";
 import { login } from "@/lib/actions";
 export default function LoginForm() {
     return <form action={login} className="space-y-2">
       <Input type="email" placeholder="name@example.com"
         name="email" required />
-      <Button type="submit" size="sm" className="w-full">Sign in with email</Button>
+      <SubmitButton type="submit" size="sm" className="w-full">Sign in with email</SubmitButton>
     </form>
   }
---
+++ components/submitButton/submitButton.jsx
@@ -0,0 +1,10 @@
+import { Loader } from "lucide-react"
+import Button from "../button/button"
+import { useFormStatus } from "react-dom"
+export default function SubmitButton(props) {
+    const {pending} = useFormStatus()
+    return <Button {...props} className={`${props.className} flex items-center justify-center space-x-2`}>
+      {pending && <Loader className="animate-spin w-4 h-4" />}
+      {props.children}
+    </Button>
+  }
