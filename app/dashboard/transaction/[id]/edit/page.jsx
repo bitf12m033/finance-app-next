a125056f0a4114e36b33da0c650b0ed0e6957bef
Umer Majeed <umajeed@vinsuite.com>
2024-06-06 18:26:47 +0500

    Edit Transaction - 404 page

---
+++ app/dashboard/transaction/[id]/edit/page.jsx
@@ -0,0 +1,22 @@
+import TransactionForm from "@/components/dashboard/transactionForm/transactionForm";
+import { createClient } from "@/lib/supabase/server"
+import { notFound } from 'next/navigation';
+
+export const metadata = {
+  title: "Edit Transaction"
+}
+export default async function Page({ params: { id } }) {
+    const supabase = createClient()
+    const { data: transaction, error } = await supabase.
+      from('transactions')
+      .select('*')
+      .eq('id', id)
+      .single()
+      if (error) notFound()
+
+    return <>
+        <h1 className="text-4xl font-semibold mb-8">Edit Transaction</h1>
+        <TransactionForm initialData={transaction} />
+    </>
+
+}
--- components/dashboard/transactionForm/transactionForm.jsx
+++ components/dashboard/transactionForm/transactionForm.jsx
@@ -12,7 +12,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { createTransaction } from "@/lib/actions";
 import FormError from "@/components/formError/formError";
-export default function TransactionForm() {
+export default function TransactionForm({initialData}) {
     const {
         register,
         handleSubmit,
@@ -22,12 +22,17 @@ export default function TransactionForm() {
       } = useForm({
         mode: "onTouched",
         resolver: zodResolver(transactionSchema),
+        defaultValues: initialData ?? {
+          created_at: new Date().toISOString().split('T')[0]
+        }
       })
     
     const router = useRouter()
     const [isSaving, setSaving] = useState(false)
     const [lastError, setLastError] = useState()
     const type = watch('type')
+    const editing = Boolean(initialData)
+
 
     const onSubmit = async (data) => {
      
@@ -35,7 +40,11 @@ export default function TransactionForm() {
         setLastError()
         try {
             
-          await createTransaction(data)
+          if (editing) {
+            // Edit action
+          } else {
+            await createTransaction(data)
+          }
           router.push('/dashboard')
         }
         catch (error) {
@@ -71,7 +80,7 @@ export default function TransactionForm() {
 
       <div>
         <Label className="mb-1">Date</Label>
-        <Input {...register("created_at")} />
+        <Input {...register("created_at")}  disabled={editing} />
        <FormError error={ errors.created_at} />
       </div>
 
