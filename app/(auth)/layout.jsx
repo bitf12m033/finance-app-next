8b84b727d02c7d0cbf53c7dd6b039a2c7c5b650e
Umer Majeed <umajeed@vinsuite.com>
2024-06-06 18:37:59 +0500

    Login page

---
+++ app/(auth)/layout.jsx
@@ -0,0 +1,16 @@
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+import { variants, sizes } from "@/lib/variants";
+export default function Layout({children}) {
+    return <main>
+      <div className="absolute left-8 top-8">
+        <Link href="/" className={`${variants['ghost']} ${sizes['base']} flex items-center space-x-2 text-sm`}>
+          <ChevronLeft class="w-4 h-4" />
+          <span>Back</span>
+        </Link>
+      </div>
+      <div className="mt-8">
+        {children}
+      </div>
+    </main>
+  }
