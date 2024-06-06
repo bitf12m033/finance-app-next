3c2b9a947eb530f37c2dd7c818296cfcb6dfcc9b
Umer Majeed <umajeed@vinsuite.com>
2024-06-06 17:10:37 +0500

    loading more feature in client component

--- components/transactionList/transactionList.jsx
+++ components/transactionList/transactionList.jsx
@@ -6,19 +6,29 @@ import TransactionItem from "../transactionItem/transactionItem"
 import TransactionSummaryItem from "../transactionSummaryItem/transactionSummaryItem"
 import { groupAndSumTransactionsByDate } from "@/lib/utils"
 import { useState } from "react"
+import { Loader } from "lucide-react"
 
-export default async function TransactionList({range , initialTransactions}) {
+export default  function TransactionList({range , initialTransactions}) {
     const [transactions, setTransactions] = useState(initialTransactions)
-    const [offset, setOffset] = useState(initialTransactions.length)
+    const [offset, setOffset] = useState(initialTransactions.length + 1)
+    const [buttonHidden, setButtonHidden] = useState(initialTransactions.length === 0)
+    const [loading, setLoading] = useState(false)
     const grouped = groupAndSumTransactionsByDate(transactions)
     
     const handleClick = async (e) => {
-        const nextTransactions = await fetchTransactions(range, offset, 10)
+        setLoading(true)
+        let nextTransactions = null
+        try {
+        nextTransactions = await fetchTransactions(range, offset, 10)
+        setButtonHidden(nextTransactions.length === 0)
         setOffset(prevValue => prevValue + 10)
         setTransactions(prevTransactions => [
-          ...prevTransactions,
-          ...nextTransactions
+            ...prevTransactions,
+            ...nextTransactions
         ])
+        } finally {
+        setLoading(false)
+        }
     }
     return (
         <div className="space-y-8">
@@ -38,9 +48,15 @@ export default async function TransactionList({range , initialTransactions}) {
                 })
             }
 
-            <div className="flex justify-center">
-                <Button variant="ghost" onClick={handleClick}>Load More</Button>
-            </div>
+            {transactions.length === 0 && <div className="text-center text-gray-400 dark:text-gray-500">No transactions found</div>}
+            {!buttonHidden && <div className="flex justify-center">
+                <Button variant="ghost" onClick={handleClick} disabled={loading}>
+                    <div className="flex items-center space-x-1">
+                        {loading && <Loader className="animate-spin" />}
+                        <div>Load More</div>
+                    </div>
+                </Button>
+            </div>}
         </div>
     )
   }
