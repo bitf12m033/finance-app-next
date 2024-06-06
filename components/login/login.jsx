9ba01cf386aaab0ee32ef19f171001e5ad7fe92b
Umer Majeed <umajeed@vinsuite.com>
2024-06-06 18:57:51 +0500

    form validation in server actions using useFormState

--- components/login/login.jsx
+++ components/login/login.jsx
@@ -2,10 +2,20 @@
 import SubmitButton from "../submitButton/submitButton";
 import Input from "../input/input";
 import { login } from "@/lib/actions";
+import { useFormState } from "react-dom";
+
+const initialState = {
+    message: '',
+    error: false
+  }
 export default function LoginForm() {
-    return <form action={login} className="space-y-2">
+    const [state, formAction] = useFormState(login, initialState)
+    return <form action={formAction} className="space-y-2">
       <Input type="email" placeholder="name@example.com"
         name="email" required />
       <SubmitButton type="submit" size="sm" className="w-full">Sign in with email</SubmitButton>
+      <p className={`${state?.error ? 'text-red-500' : 'text-green-500'} text-sm text-center`}>
+      {state?.message}
+    </p>
     </form>
   }
