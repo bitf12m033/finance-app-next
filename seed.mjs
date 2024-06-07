a795bf56df9ac88ebde9d5eeb77f6340ba264839
Umer Majeed <umajeed@vinsuite.com>
2024-06-07 18:01:24 +0500

    user seed

--- seed.mjs
+++ seed.mjs
@@ -13,13 +13,40 @@ const categories = [
   'Housing', 'Transport', 'Health', 'Food', 'Education', 'Other'
 ]
 
-async function seed() {
-  let transactions = []
+async function seedUsers() {
+  for (let i = 0; i < 20; i++) {
+    try {
+      const { error } = await supabase.auth.admin.createUser({
+        email: faker.internet.email(),
+        password: 'password',
+      })
 
-  for (let i = 0; i < 10; i++) {
+      if (error) {
+        throw new Error(error)
+      }
+
+      console.log(`User added`)
+    } catch (e) {
+      console.error(`Error adding user`)
+    }
+  }
+}
+
+async function seed() {
+  await seedUsers()
+  let transactions = []
+  const { data: { users }, error: listUsersError } = await supabase.auth.admin.listUsers()
+
+  if (listUsersError) {
+    console.error(`Cannot list users, aborting`)
+    return
+  }
+  const userIds = users?.map(user => user.id)
+
+  for (let i = 0; i < 100; i++) {
     const created_at = faker.date.past()
     let type, category = null
-
+    const user_id = faker.helpers.arrayElement(userIds)
     const typeBias = Math.random()
 
     if (typeBias < 0.80) {
@@ -64,6 +91,7 @@ async function seed() {
       type,
       description: faker.lorem.sentence(),
       category,
+      user_id
     })
   }
 
@@ -74,6 +102,7 @@ async function seed() {
     console.error('Error inserting data')
   } else {
     console.log('Data inserted')
+    console.log(`${transactions.length} transactions stored`)
   }
 }
 
