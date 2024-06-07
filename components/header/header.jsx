75ced308814905f4b6075e458851dec5a0964b3f
Umer Majeed <umajeed@vinsuite.com>
2024-06-07 18:55:33 +0500

    associate images to user via metadata

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -12,6 +12,7 @@ const Header = async ({className}) => {
   const theme = useServerDarkMode()
   const supabase = createClient()
   const { data:{user}, error} = await supabase.auth.getUser()
+  console.log(user)
   return (
     <header className={`flex justify-between items-center ${className}`}>
         <Link href={'/dashboard'} className='text-xl  hover:underline underline-offset-8 decoration-2'>
