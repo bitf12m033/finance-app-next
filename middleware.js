efd53d7e713035f776b6075c654d70f3b461402f
Umer Majeed <umajeed@vinsuite.com>
2024-06-07 16:48:56 +0500

    Protecting routes in middleware

--- middleware.js
+++ middleware.js
@@ -1,10 +1,22 @@
 import { NextResponse } from 'next/server'
 import { updateSession } from './lib/supabase/middleware'
+import { createClient } from './lib/supabase/server'
 
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request) {
-  return await updateSession(request)
+
+    const { data: { user }} = await createClient().auth.getUser()
+
+    if (!user && request.nextUrl.pathname.startsWith('/dashboard')) {
+      return Response.redirect(new URL('/login', request.url))
+    }
+  
+    if (user && request.nextUrl.pathname.startsWith('/login')) {
+      return Response.redirect(new URL('/dashboard', request.url))
+    }
+  
+    return await updateSession(request)
 }
 
 export const config = {
