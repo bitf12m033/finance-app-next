96660105f06dc00ed55e350194fd5dd5664f9dfe
Umer Majeed <umajeed@vinsuite.com>
2024-06-07 18:47:06 +0500

    upload image to supabase

--- app/dashboard/settings/avatar/page.jsx
+++ app/dashboard/settings/avatar/page.jsx
@@ -1,3 +1,16 @@
+'use client'
+import Input from "@/components/input/input"
+import SubmitButton from "@/components/submitButton/submitButton"
+import { uploadAvatar } from "@/lib/actions"
+
 export default function Page() {
-    return (<>Avatar</>)
-  }
+  return <>
+    <h1 className="text-4xl font-semibold mb-8">
+      Avatar
+    </h1>
+    <form className="space-y-4" action={uploadAvatar}>
+      <Input type="file" name="file" id="file" />
+      <SubmitButton>Upload Avatar</SubmitButton>
+    </form>
+  </>
+}
